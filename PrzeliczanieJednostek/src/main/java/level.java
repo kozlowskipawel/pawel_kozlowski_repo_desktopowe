046210fec0f2f160d102dac1d57ec41b08b7765f
kozlowskipawel <kozlowskipawel_1@wp.pl>
2022-03-15 19:17:22 +0100

    dodanie

---
+++ PrzeliczanieJednostek/src/main/java/level.java
@@ -0,0 +1,17 @@
+
+import java.util.logging.Level;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kozlowskipawel
+ */
+class level {
+
+    static Level SEVERE;
+    
+}
